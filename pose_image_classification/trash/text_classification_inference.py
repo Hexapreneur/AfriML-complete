c724c7fd7803f682e51e054f79645319925800e8
Hexapreneur <the.hexapreneur@gmail.com>
2024-07-14 19:33:09 +0300

    first upload

---
+++ pose_image_classification/trash/text_classification_inference.py
@@ -0,0 +1,22 @@
+from transformers import pipeline
+import json
+
+
+def text_inference(text):
+    try:
+        with open('static/train_text_classes.json', 'r') as f:
+            classes = json.load(f)
+        classifier = pipeline('text-classification', model='static/text_classification_model')
+        result = classifier(text)
+        
+        final_result = result[0]
+        label = ''
+        for k, v in final_result.items():
+            if k == 'label':
+                label = classes[int(v.split('_')[-1])]
+
+        final_result['label'] = label
+
+        return True, final_result, classes
+    except Exception as e:
+        return False, str(e)
